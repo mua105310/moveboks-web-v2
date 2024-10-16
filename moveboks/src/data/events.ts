20fc5e9244690be5396b841764cd996af191e4b4
Thor <43585332+mua105310@users.noreply.github.com>
2024-10-16 12:28:00 +0200

    updates
    - Adding metModel and dummaydata

--- moveboks/src/data/events.ts
+++ moveboks/src/data/events.ts
@@ -1,8 +1,9 @@
 import { EventModel } from "@/models/event";
 
-const roskilde2024 = {
+export const roskilde2024: EventModel = {
     id: "roskilde-2024",
     title: "Roskilde Festival 2024",
+    path: "lej-soundboks-til-roskilde-festival-2024",
     description: "Lej en soundboks til Roskilde Festival 2024",
     color: "#FF0000",
     packagesID: ['ros-basis', 'ros-plus', 'ros-premium'],
@@ -11,12 +12,18 @@ const roskilde2024 = {
         fromDate: new Date("2024-06-29"),
         toDate: new Date("2024-07-06"),
         showDate: new Date("2024-06-29"),
+    },
+    meta: {
+        title : "Lej en soundboks til Roskilde Festival 2024",
+        description: "Lej en soundboks til Roskilde Festival 2024",
+        keywords: "Roskilde Festival, Soundboks, lej, 2024",
     }
 }
 
-const smukfest2024 = {
+export const smukfest2024: EventModel = {
     id: "smukfest-2024",
     title: "Smukfest 2024",
+    path: "lej-soundboks-til-smukfest-2024",
     description: "Lej en soundboks til Smukfest 2024",
     color: "#FF0000",
     packagesID: ['smuk-basis', 'smuk-plus', 'smuk-premium'],
@@ -25,6 +32,11 @@ const smukfest2024 = {
         fromDate: new Date("2024-08-07"),
         toDate: new Date("2024-08-11"),
         showDate: new Date("2024-08-07"),
+    },
+    meta: {
+        title : "Lej en soundboks til Smukfest 2024",
+        description: "Lej en soundboks til Smukfest 2024",
+        keywords: "Smukfest, Soundboks, lej, 2024",
     }
 }
 
--- moveboks/src/models/event.ts
+++ moveboks/src/models/event.ts
@@ -1,18 +1,27 @@
 export type EventModel = {
     id: string,
     title: string,
+    path: string,
     description: string,
     color: string,
     packagesID: string[],
-    dateInfo: datesModel,
+    dateInfo: DatesModel,
     images: string[],
+    meta: MetaModel,
 }
 
-export type datesModel = {
+type DatesModel = {
     fromDate: Date,
     toDate: Date,
     showDate?: Date,
 }
+
+export type MetaModel = {
+    title: string;                  // Used for both <title> and og:title (can be the same)
+    description: string;            // Used for both <meta description> and og:description (can be the same)
+    keywords?: string;              // For SEO purposes (less important today)
+};
+
 
 
 
