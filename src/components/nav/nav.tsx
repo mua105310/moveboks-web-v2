2e322a4e3b69e00c567a8a1599f2be515135283d
Thor <43585332+mua105310@users.noreply.github.com>
2024-10-26 16:32:41 +0200

    .
    .

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,11 +1,16 @@
 "use client";
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import Logo from '@/app/assets/logo.png'
+import Logo from '@/app/assets/logo.png';
 import Link from 'next/link';
 import { HiMenuAlt4 } from 'react-icons/hi';
-export default function Nav() {
-    
+import { EventModel } from "@/models/event";
+
+interface navProps {
+  events: EventModel[];
+}
+
+export default function Nav({events}: navProps) {
     const [open, setOpen] = useState(false);
 
     function toggleOpen() {
@@ -13,34 +18,37 @@ export default function Nav() {
     }
 
     return (
-        <div className="absolute top-0 w-full p-10 z-50">
-            <div className='flex flex-row justify-between items-center'>
-                {/* Logo start */}
-                <div>
-                    <Link
-                        href='/'
-                    >
-                    <div className='flex items-center cursor-pointer' >
-                        <Image 
-                            src={Logo}
-                            alt='logo'
-                            width={130}
-                            priority={true}
-                        />
-            
-                        <div className='hidden md:block ml-4 h-7 border-r border-white opacity-30' />
-                        <div className='ml-4 hidden md:block leading-4 tracking-[1.5px]' style={{ fontSize: "9px" }}>SKAB DIT<br /><strong>MOMENT</strong></div>
+        <div className="absolute top-0 w-full h-full z-50">
+            <div className='p-10 relative z-20'>
+                <div className='flex flex-row justify-between items-center'>
+                    {/* Logo start */}
+                    <div>
+                        <Link href='/'>
+                            <div className='flex items-center cursor-pointer'>
+                                <Image 
+                                    src={Logo}
+                                    alt='logo'
+                                    width={130}
+                                    priority={true}
+                                />
+                                <div className='hidden md:block ml-4 h-7 border-r border-white opacity-30' />
+                                <div className='ml-4 hidden md:block leading-4 tracking-[1.5px]' style={{ fontSize: "9px" }}>SKAB DIT<br /><strong>MOMENT</strong></div>
+                            </div>
+                        </Link>
                     </div>
-                    </Link>
+                    {/* Logo end */}
+                        
+                    {/* Menu Icon */}
+                    <div onClick={toggleOpen} className="cursor-pointer">
+                        <HiMenuAlt4 className={`text-white transition-all duration-500 text-3xl ${open ? 'rotate-90' : ''} hover:text-[var(--secondary)]`} />
+                    </div>
                 </div>
-                {/* Logo end */}
-                    
-                {/* Nav start */}
-                <div onClick={toggleOpen}>
-                    <HiMenuAlt4 className={`text-white transition-all duration-500 text-3xl ${open && 'rotate-90'} cursor-pointer hover:text-[var(--secondary)]`} />
-                </div>
-                {/* Nav end */}
+            </div>
+            
+            {/* Nav menu */}
+            <div className={`absolute top-0 left-0 w-full h-full bg-[var(--background)] transition-transform duration-700 ease-in-out ${open ? 'translate-y-0' : '-translate-y-full'} z-10`}>
+
             </div>
         </div>
-    )
+    );
 }
