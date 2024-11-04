5fd6dc2cc24095086e1d95940d1c80e7d24c0fdf
Thor <43585332+mua105310@users.noreply.github.com>
2024-11-04 13:36:33 +0100

    updates
    update gap-2 beacuse lighthouse saied it

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SocialMediaTag from "../social/social-media-tag";
 import { EventModel } from "@/models/event";
+import Link from 'next/link';
 
 interface FooterProps {
   events: EventModel[];
@@ -31,7 +32,7 @@ export default function Footer({ events }: FooterProps) {
             item && (
               <React.Fragment key={item.title}>
                 {!item.subItems ? (
-                  <a
+                  <Link
                     href={item.path}
                     className="relative inline-block transition-colors duration-300 group"
                   >
@@ -39,10 +40,10 @@ export default function Footer({ events }: FooterProps) {
                     <div
                       className="absolute left-0 top-1/2 transform -translate-y-1/2 w-0 h-[3px] bg-[var(--secondary)] transition-all duration-700 ease-in-out group-hover:w-full"
                     />
-                  </a>
+                  </Link>
                 ) : (
                   item.subItems.map((subItem) => (
-                    <a
+                    <Link
                       key={subItem.title}
                       href={subItem.path}
                       className="relative inline-block transition-colors duration-300 group"
@@ -51,7 +52,7 @@ export default function Footer({ events }: FooterProps) {
                       <div
                         className="absolute left-0 top-1/2 transform -translate-y-1/2 w-0 h-[3px] bg-[var(--secondary)] transition-all duration-700 ease-in-out group-hover:w-full"
                       />
-                    </a>
+                    </Link>
                   ))
                 )}
               </React.Fragment>
@@ -64,10 +65,10 @@ export default function Footer({ events }: FooterProps) {
 
         {/* Contact and Social Media Section */}
         <div className="flex justify-between items-center w-full md:flex-1">
-          <div className="flex flex-col space-y-1 text-[10px]">
-            <a href="tel:+4520557865">+45 20 55 78 65</a>
-            <a href="mailto:Kontakt@moveboks.dk">Kontakt@moveboks.dk</a>
-            <a href="#">CVR: 41417749</a>
+          <div className="flex flex-col space-y-1 text-[10px] gap-2">
+            <Link href="tel:+4520557865">+45 20 55 78 65</Link>
+            <Link href="mailto:Kontakt@moveboks.dk">Kontakt@moveboks.dk</Link>
+            <Link href="#">CVR: 41417749</Link>
           </div>
           <div>
             <SocialMediaTag />
@@ -82,25 +83,25 @@ export default function Footer({ events }: FooterProps) {
       <div className="max-[600px]:flex-col flex justify-between text-xs ">
         <div className="flex">
           <p className="opacity-50">Executed by</p>
-          <a
+          <Link
             className="ml-1 hover:text-[var(--secondary)] transition-colors duration-300"
             href="https://datacvr.virk.dk/enhed/virksomhed/41417749?fritekst=moveboks&sideIndex=0&size=10"
           >
             Moveboks
-          </a>
+          </Link>
         </div>
         <div className="flex flex-wrap max-[600px]:mt-5 gap-2">
-          <a href="/handelsbetingelser" className="hover:text-[var(--secondary)] transition-colors duration-300">
+          <Link href="/handelsbetingelser" className="hover:text-[var(--secondary)] transition-colors duration-300">
             Handelsbetingelser
-          </a>
+          </Link>
           <div className="h-full w-[1px] bg-white ml-2 mr-2 opacity-15" />
-          <a href="/persondatapolitik" className="hover:text-[var(--secondary)] transition-colors duration-300">
+          <Link href="/persondatapolitik" className="hover:text-[var(--secondary)] transition-colors duration-300">
             Persondatapolitik
-          </a>
+          </Link>
           <div className="h-full w-[1px] bg-white ml-2 mr-2 opacity-15" />
-          <a href="/kontakt" className="hover:text-[var(--secondary)] transition-colors duration-300">
+          <Link href="/kontakt" className="hover:text-[var(--secondary)] transition-colors duration-300">
             Kontakt
-          </a>
+          </Link>
         </div>
       </div>
     </div>
