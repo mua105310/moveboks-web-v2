96868bcbe7e5f72a01dff869e827572f82b709e4
Thor <43585332+mua105310@users.noreply.github.com>
2024-11-15 10:26:57 +0100

    .
    .

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -9,38 +9,38 @@ export default function Card({item, onClick}: CardModel) {
         border border-white/20
         text-white
         transform h-[200px]
-        overflow-hidden rounded-lg"
+        overflow-hidden rounded-lg
+        relative"
       onClick={onClick}
     >
-      <div className="flex h-full card">
-        <div className="flex flex-col flex-grow p-0">
-          <div className="flex-shrink-0 p-4">
-            <h2 className="text-[20px] font-bold" style={{textShadow: '0 0 6px rgba(255, 255, 255, 0.6)'}}>
-              {item.title}
-            </h2>
-            <p className="text-[12px] line-clamp-2 mt-1">{item.longDescription}</p>
-          </div>
-          
-          <div className="mt-auto flex justify-between items-center bg-[#151515] p-4">
-            <span className="font-medium text-white text-[10px] sm:text-[12px] lg:text-[10px]">fra 3099 kr.</span>
-            <button className="bg-blue-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors text-[12px]">
-              Tilføj til
-            </button>
-          </div>
+      {item.images && item.images[0] && (
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src={item.images[0]}
+            alt={`Image for ${item.title}`}
+            fill
+            className="object-cover opacity-80 hover:opacity-30 transition-opacity"
+            priority
+            draggable={false}
+          />
+          <div className="absolute inset-0 bg-gradient-to-t " />
         </div>
+      )}
 
-        {item.images && item.images[0] && (
-          <div className="relative w-32 h-full flex-shrink-0">
-            <Image
-              src={item.images[0]}
-              alt={`Image for ${item.title}`}
-              fill
-              className="object-contain p-2 hover:scale-105 transition-transform"
-              priority
-              draggable={false}
-            />
-          </div>
-        )}
+      <div className="relative h-full flex flex-col p-4 bg-[#151515]/80">
+        <div className="flex-shrink-0">
+          <h2 className="text-[20px] font-bold" style={{textShadow: '0 0 6px rgba(255, 255, 255, 0.6)'}}>
+            {item.title}
+          </h2>
+          <p className="text-[12px] line-clamp-2 mt-1">{item.longDescription}</p>
+        </div>
+        
+        <div className="mt-auto flex justify-between items-center">
+          <span className="font-medium text-white text-[10px] sm:text-[12px] lg:text-[10px]">fra 3099 kr.</span>
+          <button className="bg-blue-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors text-[12px]">
+            Tilføj til
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/data/packages.ts
+++ src/data/packages.ts
@@ -102,7 +102,7 @@ export const packagesBasisSmukfest: PackageModel = {
     predefinedLocationAndTime: true,
     shortDescription: "Basispakken til Smukfest uden dækning",
     longDescription: "Ved køb af basispakken står du selv for ansvaret i tilfælde af tyveri og skade",
-    images: [],
+    images: ["https://imagedelivery.net/cITCNbiqfZeimGDoNY0eMg/2d809dfb-51fc-4503-b906-0936213dc800/public"],
     features: ["Ingen dækning"],
     options: smukProducts,
 };
@@ -113,7 +113,7 @@ export const packagesPlusSmukfest: PackageModel = {
     predefinedLocationAndTime: true,
     shortDescription: "Inkluder skadeforsikring",
     longDescription: "Ved køb af pluspakken er du dækket i tilfælde af skade",
-    images: [],
+    images: ["https://imagedelivery.net/cITCNbiqfZeimGDoNY0eMg/edfb5303-e5e2-4c2d-eae3-e0a8ad18fb00/public"],
     features: ["Skadeforsikring"],
     options: smukProducts.map(product => ({
         ...product,
@@ -127,7 +127,7 @@ export const packagesPremiumSmukfest: PackageModel = {
     predefinedLocationAndTime: true,
     shortDescription: "Inkluder skadeforsikring og tyveriforsikring",
     longDescription: "Ved køb af pluspakken er du dækket i tilfælde af skade og tyveri",
-    images: [],
+    images: ["https://imagedelivery.net/cITCNbiqfZeimGDoNY0eMg/3bd01de0-a395-42c0-ca10-7eedbf177500/public"],
     features: ["Skadeforsikring", "Tyveriforsikring"],
     options: smukProducts.map(product => ({
         ...product,
