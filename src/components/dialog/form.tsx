bd4be464b28d7b2209a9eeaa5165bffdd591ad9f
Thor <43585332+mua105310@users.noreply.github.com>
2024-11-29 00:46:15 +0100

    .
    .

--- src/components/dialog/form.tsx
+++ src/components/dialog/form.tsx
@@ -2,7 +2,7 @@ export default function Form() {
     return (
         <div className="">
             <p className="pb-5">Informationer</p>
-            <div>
+            <div className="text-sm">
                 <input type="text" placeholder="Navn" className="w-full p-3 border border-white/20 rounded-lg mb-5 bg-white/5 outline-none"/>
                 <input type="text" placeholder="Email" className="w-full p-3 border border-white/20 rounded-lg mb-5 bg-white/5 outline-none"/>
                 <input type="text" placeholder="Gentag email" className="w-full p-3 border border-white/20 rounded-lg mb-5 bg-white/5 outline-none"/>
