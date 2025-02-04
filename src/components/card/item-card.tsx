7defc9f93e2ae09c60a839448074d11f638502f4
Thor <43585332+mua105310@users.noreply.github.com>
2025-02-04 20:16:08 +0100

    updates

---
+++ src/components/card/item-card.tsx
@@ -0,0 +1,16 @@
+import { ProductConstraintModel } from "@/internal/package";
+
+export default function ItemCard({ item }: { item: ProductConstraintModel }) {
+  return (
+    <div>
+        {/* Image */}
+        <div className="relative w-full h-1/2">
+            
+        </div>
+        {/* Content */}
+        <div>
+
+        </div>
+    </div>
+  );
+}
