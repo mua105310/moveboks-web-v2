fb34ee1cea8e89649be82b364464a09aed4446a5
Thor <43585332+mua105310@users.noreply.github.com>
2025-02-04 19:32:22 +0100

    .

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+// src/config/config.ts
+
+export interface AppConfig {
+    API_ENDPOINT: string;
+    baseUrl?: string;
+  }
+  
+  const productionConfig: AppConfig = {
+    API_ENDPOINT: 'https://moveboks.dk/v1',
+    baseUrl: 'https://moveboks.dk',
+  };
+  
+  const localConfig: AppConfig = {
+    API_ENDPOINT: 'http://127.0.0.1:3000/v1',
+    baseUrl: 'http://localhost:3000/',
+  };
+  
+  const isProduction = process.env.NODE_ENV === 'production';
+  
+  export const config: AppConfig = isProduction ? productionConfig : localConfig;
+  
