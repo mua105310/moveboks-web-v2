b1623f9826b6598a7fb9725ef55060dac259b6df
Thor <43585332+mua105310@users.noreply.github.com>
2025-02-05 15:48:28 +0100

    updates

---
+++ src/components/card/package-card.tsx
@@ -0,0 +1,13 @@
+import { PackageModel} from "@/internal/models/package";
+
+export interface PackageCardProps {
+  pack: PackageModel;
+}
+
+export default function PackageCard({ pack }: PackageCardProps) {
+  return (
+    <div>
+        
+    </div>
+  );
+}
---
+++ src/components/card/product-card.tsx
@@ -0,0 +1,13 @@
+import { ProductConstraintModel } from "@/internal/models/package";
+
+export interface PackageCardProps {
+  product: ProductConstraintModel;
+}
+
+export default function PackageCard({ product }: PackageCardProps) {
+  return (
+    <div>
+        
+    </div>
+  );
+}
